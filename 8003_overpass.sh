56127b30b2fe1374a9091cd536b4d763a1fd7c11
yuiseki <yuiseki@gmail.com>
2025-07-18 16:24:32 +0900

    Update 8003_overpass.sh

--- 8003_overpass.sh
+++ 8003_overpass.sh
@@ -1,12 +1,16 @@
+export OVERPASS_PLANET_PREPROCESS="osmium cat /data/planet.osm.pbf -o /db/planet.osm.bz2 --overwrite"
+
 docker run \
     -d \
+    --memory=80g \
     -e OVERPASS_META=yes \
     -e OVERPASS_MODE=init \
-    -e OVERPASS_PLANET_URL=http://download.geofabrik.de/europe/monaco-latest.osm.bz2 \
-    -e OVERPASS_DIFF_URL=http://download.openstreetmap.fr/replication/europe/monaco/minute/ \
+    -e OVERPASS_PLANET_URL=file:///db/planet.osm.bz2 \
+    -e OVERPASS_DIFF_URL=https://planet.openstreetmap.org/replication/minute/ \
     -e OVERPASS_RULES_LOAD=10 \
-    -v ./data/overpass_db/:/db \
+    -e OVERPASS_PLANET_PREPROCESS \
+    -v ./data/planet-latest.osm.pbf:/data/planet.osm.pbf:ro \
+    -v ./data/overpass_db_planet/:/db \
     -p 8003:80 \
-    --name overpass_monaco \
+    --name overpass_planet \
     wiktorn/overpass-api
-
