c09bfc500dac5bf809c6cabe314a9c20cc63e1c4
yuiseki <yuiseki@gmail.com>
2025-09-22 17:45:24 +0900

    Update 8001_nominatim.sh

--- 8001_nominatim.sh
+++ 8001_nominatim.sh
@@ -6,10 +6,12 @@ docker run \
     --rm \
     -it \
     -e PBF_PATH=/nominatim/data/planet-latest.osm.pbf \
-    -e IMPORT_STYLE=admin \
-    -v nominatim-flatnode:/nominatim/flatnode \
-    -v nominatim-postgresql:/var/lib/postgresql/16/main \
-    -v ./data:/nominatim/data \
+    -e IMPORT_WIKIPEDIA=true \
+    -v $(pwd)/data/nominatim/flatnode:/nominatim/flatnode \
+    -v $(pwd)/data/nominatim/postgres:/var/lib/postgresql/16/main \
+    -v $(pwd)/data/nominatim/data:/nominatim/data \
+    -v $(pwd)/data/nominatim/wikimedia-importance.csv.gz:/nominatim/wikimedia-importance.csv.gz \
+    -v /everything/osm/planet/planet-latest.osm.pbf:/nominatim/data/planet-latest.osm.pbf:ro \
     -p 8001:8080 \
     --name nominatim_planet \
     mediagis/nominatim:5.1
