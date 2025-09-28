cb35b0acd46803c763c8cead9bf841b20733e3b5
yuiseki <yuiseki@gmail.com>
2025-09-28 19:34:07 +0900

    update 8002_overpass.sh

--- 8002_overpass.sh
+++ 8002_overpass.sh
@@ -3,13 +3,14 @@ docker run \
   --memory-swap=64g \
   --shm-size=16g \
   --rm -it \
-  --name overpass_init \
+  --name overpass_planet_init \
   -e OVERPASS_MODE=init \
+  -e OVERPASS_STOP_AFTER_INIT=true \
   -e OVERPASS_META=yes \
+  -e OVERPASS_RULES_LOAD=80 \
   -e OVERPASS_PLANET_URL=file:///data/planet.osm.pbf \
   -e OVERPASS_COMPRESSION=lz4 \
-  -e OVERPASS_RULES_LOAD=50 \
-  -e OVERPASS_PLANET_PREPROCESS='osmium cat /data/planet.osm.pbf -o /db/planet.osm.bz2 --overwrite' \
+  -e OVERPASS_PLANET_PREPROCESS='osmium cat /data/planet.osm.pbf -f osm -o /db/planet.osm.bz2 --overwrite' \
   -v $(pwd)/data/overpass/db_planet/:/db \
   -v $(pwd)/data/overpass/planet-latest.osm.pbf:/data/planet.osm.pbf:ro \
   wiktorn/overpass-api
