b87e2fc29261bda68e99c130277b5ae91d11969b
yuiseki <yuiseki@gmail.com>
2025-09-28 12:58:46 +0900

    WIP

--- 8002_overpass_monaco.sh
+++ 8002_overpass_monaco.sh
@@ -1,12 +1,14 @@
 docker run \
-    -d \
-    -e OVERPASS_META=yes \
-    -e OVERPASS_MODE=init \
-    -e OVERPASS_PLANET_URL=http://download.geofabrik.de/europe/monaco-latest.osm.bz2 \
-    -e OVERPASS_DIFF_URL=http://download.openstreetmap.fr/replication/europe/monaco/minute/ \
-    -e OVERPASS_RULES_LOAD=10 \
-    -v ./data/overpass/db_monaco/:/db \
-    -p 8003:80 \
-    --name overpass_monaco \
-    wiktorn/overpass-api
+  --memory=64g \
+  --memory-swap=64g \
+  --shm-size=16g \
+  --rm -it \
+  --name overpass_monaco_init \
+  -e OVERPASS_MODE=init \
+  -e OVERPASS_META=yes \
+  -e OVERPASS_PLANET_URL=http://download.geofabrik.de/europe/monaco-latest.osm.pbf \
+  -e OVERPASS_DIFF_URL=http://download.openstreetmap.fr/replication/europe/monaco/minute/ \
+  -e OVERPASS_RULES_LOAD=50 \
+  -v $(pwd)/data/overpass/db_monaco/:/db \
+  wiktorn/overpass-api
 
