a58c8a873360bb797e0e7303bba8ce33dadb9f4d
yuiseki <yuiseki@gmail.com>
2025-10-01 06:53:49 +0900

    WIP

--- 8001_nominatim_monaco.sh
+++ 8001_nominatim_monaco.sh
@@ -1,10 +1,11 @@
 docker run \
   --rm -it \
-  --name nominatim_monaco_init \
+  --name nominatim_monaco \
   -e PBF_URL=https://download.geofabrik.de/europe/monaco-latest.osm.pbf \
   -e NOMINATIM_FLATNODE_FILE=/nominatim/flatnode/flatnode.file \
-  -e IMPORT_WIKIPEDIA=true \
+  -e IMPORT_WIKIPEDIA=false \
   -v $(pwd)/data/nominatim/monaco/flatnode:/nominatim/flatnode \
   -v $(pwd)/data/nominatim/monaco/postgres:/var/lib/postgresql/16/main \
   -v $(pwd)/data/nominatim/monaco/data:/nominatim/data \
+  -p 8001:8080 \
   mediagis/nominatim:5.1
--- 8001_nominatim_planet_admin.sh
+++ 8001_nominatim_planet_admin.sh
@@ -4,12 +4,13 @@ docker run \
   --shm-size=64g \
   --ulimit nofile=1048576:1048576 \
   --rm -it \
-  --name nominatim_planet_admin_init \
+  --name nominatim_planet_admin \
   -e PBF_PATH=/nominatim/data/planet-latest.osm.pbf \
   -e NOMINATIM_FLATNODE_FILE=/nominatim/flatnode/flatnode.file \
   -e IMPORT_STYLE=admin \
-  -e IMPORT_WIKIPEDIA=true \
+  -e IMPORT_WIKIPEDIA=false \
   -v $(pwd)/data/nominatim/planet_admin/flatnode:/nominatim/flatnode \
   -v $(pwd)/data/nominatim/planet_admin/postgres:/var/lib/postgresql/16/main \
   -v $(pwd)/data/nominatim/planet_admin/data:/nominatim/data \
+  -p 8001:8080 \
   mediagis/nominatim:5.1
