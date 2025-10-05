d410dd940400666ee858c6d153d865c8968eb775
yuiseki <yuiseki@gmail.com>
2025-10-05 09:28:40 +0900

    Update 8003_valhalla.sh

--- 8003_valhalla.sh
+++ 8003_valhalla.sh
@@ -2,7 +2,9 @@ docker run \
   --memory=64g \
   --memory-swap=64g \
   --shm-size=16g \
-  --name valhalla_gis-ops \
+  --ulimit nofile=1048576:1048576 \
   --rm -it \
+  --name valhalla_gis-ops \
+  -e serve_tiles=False \
   -v $PWD/data/valhalla:/custom_files \
   ghcr.io/nilsnolde/docker-valhalla/valhalla:latest
